52d3d7ff3f62ff46da5f6ea6b360766f3cbaa5e5
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2021-04-24 16:47:22 -0500

    Create lm35.ino

---
+++ lm35.ino
@@ -0,0 +1,23 @@
+int pin = A0 ;              // Este pin lee la temperatura
+int umbral = 28 ;            // Indica la temperatura en la que enciende el ventilador
+const int control = 9 ;    // Controla el ventilador
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(control, OUTPUT) ;
+}
+
+void loop()
+{
+    int lectura = analogRead(pin);
+    float voltaje = (5.0 * lectura) /1023; // Atencion aqui
+    float temp = voltaje * 100;
+    Serial.println(temp) ;
+    if (temp >= umbral)
+        digitalWrite(control, HIGH);
+    else
+       digitalWrite(control, LOW);
+
+    delay(200);
+}
